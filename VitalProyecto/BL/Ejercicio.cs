6fe9f585e3b6ca23d03d59a280a7a872549e98b0
juandi perez <juandi199847@hotmail.com>
2018-10-20 22:37:31 -0600

    Avance rutinas

---
+++ VitalProyecto/BL/Ejercicio.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class Ejercicio
+    {
+        public Ejercicio(int clave, string nombre, string categoria, string descripcion)
+        {
+            Clave = clave;
+            Nombre = nombre;
+            Categoria = categoria;
+            Descripcion = descripcion;
+        }
+
+        public Ejercicio() { }
+
+        public int Clave { get; set; }
+
+        public String Nombre { get; set; }
+
+        public String Categoria { get; set; }
+
+        public String Descripcion { get; set; }
+
+        public int Repeticiones { get; set; }
+
+        public int Series { get; set; }
+
+    }
+}
