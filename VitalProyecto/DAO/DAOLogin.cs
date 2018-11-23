2a35b69897b8742e85dea70ad4f24b0c42c16140
tannia elizondo cespedes <tannia130@hotmail.com>
2018-11-22 19:48:40 -0600

    Nuevos cambios

---
+++ VitalProyecto/DAO/DAOLogin.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TO;
+
+namespace DAO
+{
+	public class DAOLogin
+	{
+		SqlConnection conexion = new SqlConnection(Properties.Settings.Default.conexion);
+
+		public TOIngreso BuscarUsuario(String correo_usuario, String contra)
+		{
+			try
+			{
+                TOIngreso usuario = new TOIngreso();
+				SqlCommand buscar = new SqlCommand("SELECT * FROM Login WHERE Nombre_usuario = @corrusu and Clave = @contus", conexion);
+				buscar.Parameters.AddWithValue("@corrusu", correo_usuario);
+				buscar.Parameters.AddWithValue("@contus", contra);
+				conexion.Open();
+				SqlDataReader lector = buscar.ExecuteReader();
+
+				if (lector.HasRows)
+				{
+					while (lector.Read())
+					{
+						usuario.nombre_usuario = lector.GetString(0);
+						usuario.clave = lector.GetString(1);
+						usuario.rol = lector.GetString(2);
+						
+					}
+					lector.Close();
+				}
+				conexion.Close();
+				return usuario;
+			}
+			catch (Exception)
+			{
+				return null;
+			}
+		}
+
+        public int registrarLogin(TOIngreso tOlogin) {
+            SqlCommand cmd = new SqlCommand("insert into Login values (@nomUs,@pass,@rol)", conexion);
+            cmd.Parameters.AddWithValue("@nomUs", tOlogin.nombre_usuario);
+            cmd.Parameters.AddWithValue("@pass", tOlogin.clave);
+            cmd.Parameters.AddWithValue("@rol", tOlogin.rol);
+
+            conexion.Open();
+
+            int res = 0;
+            try {
+                res = cmd.ExecuteNonQuery();
+                conexion.Close();
+            }
+            catch (Exception e) {
+                conexion.Close();
+            }
+            return res;
+        }
+	}
+}
