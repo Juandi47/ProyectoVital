db8475c9745fce63a7f1646110087429b871e9a6
Yoselyn Pérez <yoselyn_p31@hotmail.com>
2018-11-24 23:06:57 -0600

    ingreso nutri

--- VitalProyecto/DAO/DAOClienteNutricion.cs
+++ VitalProyecto/DAO/DAOClienteNutricion.cs
@@ -33,7 +33,7 @@ namespace DAO
                 cmd.Parameters.AddWithValue("@ape2", tOCliente.Apellido2);
                 cmd.Parameters.AddWithValue("@rol", "ClienteNutricion");
 
-                
+
                 cmd2.Parameters.AddWithValue("@fechNac", tOCliente.Fecha_Nacimiento);
                 cmd2.Parameters.AddWithValue("@sexo", tOCliente.Sexo);
                 cmd2.Parameters.AddWithValue("@estCiv", tOCliente.Estado_Civil);
@@ -41,7 +41,7 @@ namespace DAO
                 cmd2.Parameters.AddWithValue("@resid", tOCliente.Residencia);
                 cmd2.Parameters.AddWithValue("@ocup", tOCliente.Ocupacion);
                 cmd2.Parameters.AddWithValue("@ced", tOCliente.Cedula);
-            cmd2.Parameters.AddWithValue("@ingres", DateTime.Now);
+                cmd2.Parameters.AddWithValue("@ingres", DateTime.Now);
 
                 if (conexion.State != ConnectionState.Open)
                 {
@@ -90,13 +90,13 @@ namespace DAO
                 cmd.Parameters.AddWithValue("@ques", HabitosAlimentario.Queso);
                 cmd.Parameters.AddWithValue("@aguacat", HabitosAlimentario.Aguacate);
                 cmd.Parameters.AddWithValue("@semil", HabitosAlimentario.Semillas);
-                
+
 
                 if (conexion.State != ConnectionState.Open)
                 {
                     conexion.Open();
                 }
-                //inserta padre
+                //inserta 
                 cmd.ExecuteNonQuery();
                 if (listaR != null)
                 {
@@ -111,6 +111,47 @@ namespace DAO
                         cmd2.ExecuteNonQuery();
                     }
                 }
+
+                conexion.Close();
+
+                return true;
+            }
+            catch (SqlException)
+            {
+                return false;
+            }
+        }
+
+        public bool GuardarAntropometria(TOAntropometria antropom, TOPorciones porcion, TODistribucionPorciones distrib)
+        {
+            String query1 = "Insert into Antropometria values(" + antropom.Cedula + "," + antropom.Talla + "," +
+                antropom.PesoIdeal + "," + antropom.Edad + "," + antropom.PMB + "," + antropom.Peso + "," + antropom.PesoMaxTeoria + ", " +
+                antropom.IMC + "," + antropom.PorcGrasaAnalizador + "," + antropom.PorcGr_Bascula + "," + antropom.GB_BI + ","
+                + antropom.GB_BD + "," + antropom.GB_PI + "," + antropom.GB_PD + "," + antropom.GB_Tronco + "," + antropom.AguaCorporal + "," +
+                antropom.MasaOsea + "," + antropom.Complexión + "," + antropom.EdadMetabolica + "," + antropom.Cintura + "," + antropom.Abdomen + "," +
+                 antropom.Cadera + "," + antropom.Muslo + "," + antropom.CBM + "," + antropom.CircunfMunneca + "," + antropom.PorcentMusculo + "," +
+                 antropom.PM_BI + "," + antropom.PM_PD + "," + antropom.PM_BD + "," + antropom.PM_PI + "," + antropom.PM_Tronco + "," + antropom.Observaciones + "," +
+                  antropom.GEB + "," + antropom.GET + "," + antropom.CHOPorc + "," + antropom.CHOGram + "," + antropom.CHO_kcal + "," + antropom.ProteinaPorc + "," +
+                  antropom.ProteinaGram + ","+ antropom.Proteinakcal +","+ antropom.GrasaPorc +"," + antropom.GrasaGram +","+ antropom.Grasakcal + "); ";
+
+            String query2 = "Insert into Porciones values("+ porcion.Cedula+","+ porcion.Leche+","+ porcion.Carne+","+ porcion.Vegetales+","+ porcion.Grasa+","+
+               porcion.Fruta+","+ porcion.Azucar+","+ porcion.Harina+","+ porcion.Suplemento + ");";
+            String query3 = "Insert into DistribucionPorcion values(" + distrib.Cedula+","+ distrib.Ayunas+","+ distrib.Desayuno+","+
+                distrib.MediaMañana+","+ distrib.Almuerzo+","+ distrib.MediaTarde+","+ distrib.Cena+","+ distrib.ColacionNocturna + ");";
+            SqlCommand cmd = new SqlCommand(query1, conexion);
+            SqlCommand cmd2 = new SqlCommand(query2, conexion);
+            SqlCommand cmd3 = new SqlCommand(query3, conexion);
+            try
+            {
+               
+                if (conexion.State != ConnectionState.Open)
+                {
+                    conexion.Open();
+                }
+                //insertar
+                cmd.ExecuteNonQuery();
+                cmd2.ExecuteNonQuery();
+                cmd3.ExecuteNonQuery();
 
                 conexion.Close();
 
